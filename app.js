f816d22595a808fbbb08d7896c840510650df70b
Zhazgul17 <azimova.zh17@gmail.com>
2023-04-06 16:44:09 +0300

    create filter function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {data as person} from './data.js';
+import {data as persons} from './data.js';
 
 const main = document.querySelector('.main_cards')
 const wrap = document.createElement('div')
@@ -10,16 +10,66 @@ function createCard(obj){
     cardDiv.className = 'card'
 
     const img = document.createElement('img')
-    img.src = obj.image;
+    img.src = obj.image
+    img.className = 'image'
+
+    const textWrap = document.createElement('div')
+    textWrap.className = 'card_text_wrap'
 
     const name = document.createElement('h2')
-    name.textContent = obj.name;
-    console.log(name)
+    name.innerText = obj.name
 
     const actor = document.createElement('p')
-    actor.innerText = `Actor: ${obj.actor}`;
+    actor.innerText = `Actor: ${obj.actor}`
 
+    const gender = document.createElement('p')
+    gender.innerText = `Gender: ${obj.gender}`
+
+    const house = document.createElement('p')
+    house.innerText = `House: ${obj.house}`
+
+    const wand = document.createElement('p')
+    wand.innerText = `Wand core: ${obj.wand.core}`
+
+    const alive = document.createElement('p')
+    obj.alive ? alive.innerText = `Alive: yes`: alive.innerText = `Alive: no`
+    
+   
     wrap.append(cardDiv)
-    cardDiv.append(img, name, actor)
+    textWrap.append (name, actor, gender, house, wand, alive)
+    cardDiv.append(img, textWrap)
 }
-createCard(person)
+
+persons.forEach((person) => {
+    createCard(person)
+})
+
+const input = document.querySelector('#name')
+const element = document.querySelectorAll('.card')
+const filtered = Object.values(element)
+console.dir(element)
+
+ input.addEventListener('input', (event) => {
+    filtered.filter (card => {
+         if(card.children[1].children[0].innerHTML.toLowerCase().includes(event.target.value.toLowerCase())){
+    console.log(card)
+    card.style.display = 'flex'
+} else {
+    card.style.display = 'none'
+}
+     })
+
+ })
+
+ const select = document.querySelector('#school')
+ select.addEventListener('change', (event) => {
+    filtered.filter (card => {
+         if(card.children[1].children[3].innerHTML.toLowerCase().includes(event.target.value.toLowerCase())){
+    console.log(card)
+    card.style.display = 'flex'
+} else {
+    card.style.display = 'none'
+}
+     })
+
+ })
